7bdb4cb809ddb19c6b3cad4d2f98d1829bde79a6
Jeff Butler <jeffgbutler@gmail.com>
2018-01-09 00:50:17 -0500

    Add lambdas overview

--- docs/index.md
+++ docs/index.md
@@ -2,9 +2,10 @@
 
 1. [Overview and Fundamentals](overview.html)
 2. [Setup Instructions](Setup.md)
-3. [Immutability Overview](immutabilityOverview.html) [Immutable Objects Exercise](Immutability.md)
-3. [XML Renderer Exercise](XmlRenderer.md)
-4. [Refactoring Exercise](Refactoring.md)
+3. [Immutability Overview](immutabilityOverview.html), [Immutable Objects Exercise](Immutability.md)
+4. [Lambdas Overview](lambdasOverview.html)
+5. [XML Renderer Exercise](XmlRenderer.md)
+6. [Refactoring Exercise](Refactoring.md)
    
 # Exercises
 For all exercises, the basic instructions are "make the tests pass".  There are TODO comments in all the classes in the "exercises.*" packages with instructions on what to do. See the links above for detailed descriptions of the exercises.
---
+++ src/test/java/examples/basics/lambdas/LambdaTest.java
@@ -0,0 +1,104 @@
+package examples.basics.lambdas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import org.junit.Test;
+
+import examples.basics.immutable.SimpleImmutableObjectWithBuilder;
+
+public class LambdaTest {
+    
+    // Lamba == Anonymous Class!
+    
+    // pre java 8 with a persistent comparator
+    @Test
+    public void testPersistentComparator() {
+        List<SimpleImmutableObjectWithBuilder> testObjects = getTestObjects();
+        
+        testObjects.sort(new TestComparator());
+        
+        assertThat(testObjects.get(0).getDescription()).isEqualTo("Ash Tree");
+        assertThat(testObjects.get(1).getDescription()).isEqualTo("Elm Tree");
+        assertThat(testObjects.get(2).getDescription()).isEqualTo("Oak Tree");
+    }
+    
+    private static class TestComparator implements Comparator<SimpleImmutableObjectWithBuilder> {
+        @Override
+        public int compare(SimpleImmutableObjectWithBuilder o1, SimpleImmutableObjectWithBuilder o2) {
+            return o1.getId().compareTo(o2.getId());
+        }
+    }
+    
+    // pre java 8 with an anonymous class
+    @Test
+    public void testAnonymousClass() {
+        List<SimpleImmutableObjectWithBuilder> testObjects = getTestObjects();
+        
+        testObjects.sort(new Comparator<SimpleImmutableObjectWithBuilder>() {
+            @Override
+            public int compare(SimpleImmutableObjectWithBuilder o1, SimpleImmutableObjectWithBuilder o2) {
+                return o1.getId().compareTo(o2.getId());
+            }
+        });
+        
+        assertThat(testObjects.get(0).getDescription()).isEqualTo("Ash Tree");
+        assertThat(testObjects.get(1).getDescription()).isEqualTo("Elm Tree");
+        assertThat(testObjects.get(2).getDescription()).isEqualTo("Oak Tree");
+    }
+
+    // java 8+ with a lambda - syntax 1
+    @Test
+    public void testLambdaSyntax1() {
+        List<SimpleImmutableObjectWithBuilder> testObjects = getTestObjects();
+        
+        testObjects.sort((o1, o2) -> o1.getId().compareTo(o2.getId()));
+        
+        assertThat(testObjects.get(0).getDescription()).isEqualTo("Ash Tree");
+        assertThat(testObjects.get(1).getDescription()).isEqualTo("Elm Tree");
+        assertThat(testObjects.get(2).getDescription()).isEqualTo("Oak Tree");
+    }
+
+    // java 8+ with a lambda - syntax 2
+    @Test
+    public void testLambdaSyntax2() {
+        List<SimpleImmutableObjectWithBuilder> testObjects = getTestObjects();
+        
+        testObjects.sort((o1, o2) -> {
+            return o1.getId().compareTo(o2.getId());
+        });
+        
+        assertThat(testObjects.get(0).getDescription()).isEqualTo("Ash Tree");
+        assertThat(testObjects.get(1).getDescription()).isEqualTo("Elm Tree");
+        assertThat(testObjects.get(2).getDescription()).isEqualTo("Oak Tree");
+    }
+
+    // java 8+ with method reference
+    @Test
+    public void testMethodReference() {
+        List<SimpleImmutableObjectWithBuilder> testObjects = getTestObjects();
+        
+        testObjects.sort(this::compare);
+        
+        assertThat(testObjects.get(0).getDescription()).isEqualTo("Ash Tree");
+        assertThat(testObjects.get(1).getDescription()).isEqualTo("Elm Tree");
+        assertThat(testObjects.get(2).getDescription()).isEqualTo("Oak Tree");
+    }
+    
+    private int compare(SimpleImmutableObjectWithBuilder o1, SimpleImmutableObjectWithBuilder o2) {
+        return o1.getId().compareTo(o2.getId());
+    }
+    
+    private List<SimpleImmutableObjectWithBuilder> getTestObjects() {
+        List<SimpleImmutableObjectWithBuilder> answer = new ArrayList<>();
+        
+        answer.add(SimpleImmutableObjectWithBuilder.of(33, "Oak Tree"));
+        answer.add(SimpleImmutableObjectWithBuilder.of(22, "Elm Tree"));
+        answer.add(SimpleImmutableObjectWithBuilder.of(11, "Ash Tree"));
+        
+        return answer;
+    }
+}
